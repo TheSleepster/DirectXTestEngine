6d990fdb4b983950c1c3f7384444c1820914d451
TheSleepster <ibjal@outlook.com>
2024-04-21 16:39:19 -0500

    attempted change to clang... can't figure out how to export compile commands without cmake or make

--- code/build.bat
+++ code/build.bat
@@ -1,19 +1,29 @@
 @echo off
 
-REM THE SYNTAX FOR DISABLING A WARNING IS -wd<error-number> EX: -wd4201
-REM -O2 for release builds
+REM 64-bit MSVC building
+REM
+REM THE SYNTAX FOR DISABLING A WARNING IS  on MSVC -wd<error-number> EX: -wd4201
+REM -O2 for release builds on MSVC
 
-Set CommonCompilerFlags=-MD -GR- -EHa- -Zi -W4 -wd4189 -wd4100
-set CommonLinkerFlags= d3d11.lib kernel32.lib user32.lib shell32.lib gdi32.lib winmm.lib msvcrt.lib
-
-REM cl %CommonCompilerFlags% \DirectX\code\*.cpp /I%CommonIncludePaths% /MD /link %CommonLinkerFlags% 
-
-REM 64-bit building
+Set MSVCCommonCompilerFlags=-MD -GR- -EHa- -Zi -W4 -wd4189 -wd4100
+set MSVCCommonLinkerFlags= d3d11.lib kernel32.lib user32.lib shell32.lib gdi32.lib winmm.lib msvcrt.lib
 
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
-cl %CommonCompilerFlags% \DirectX\code\*.cpp /I%CommonIncludePaths% /link %CommonLinkerFlags%
+cl %MSVCCommonCompilerFlags% \DirectX\code\*.cpp /I%MSVCCommonIncludePaths% /link %MSVCCommonLinkerFlags%
 popd
+
+
+REM CLANG 64-bit
+
+
+REM Set ClangCommonCompilerFlags=-MJ 
+REM Set ClangCommonLinkerFlags=-lkernel32 -luser32 -lshell32 -lgdi32 -lwinmm 
+REM
+REM IF NOT EXIST ..\build mkdir ..\build
+REM pushd ..\build
+REM clang++ \DirectX\code\*.cpp -MJ%ClangCommonCompierFlags% %ClangCommonLinkerFlags%
+REM popd
 
 @echo ====================
 @echo Compilation Finished
